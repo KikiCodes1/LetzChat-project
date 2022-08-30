fa3ca0e1e4a6ceb71b049951807b9116431cb2ca
KikiCodes1 <95891807+KikiCodes1@users.noreply.github.com>
2022-08-30 19:56:33 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyARTWYMfsypwDAH6sceYvhuFQYgTLXck8w",
+    authDomain: "kwitter-7a3de.firebaseapp.com",
+    databaseURL: "https://kwitter-7a3de-default-rtdb.firebaseio.com",
+    projectId: "kwitter-7a3de",
+    storageBucket: "kwitter-7a3de.appspot.com",
+    messagingSenderId: "235214242469",
+    appId: "1:235214242469:web:3921797c75f41800eac34c"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
